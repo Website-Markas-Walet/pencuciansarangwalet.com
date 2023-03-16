d407775e3661d57181a9b5470e8494f5f675e21a
rahmaa <113582506+rahmaazs@users.noreply.github.com>
2023-03-16 11:00:34 +0700

    google tag manager

--- wp-login.php
+++ wp-login.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en-US">
 	<head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NPLJCRC');</script>
+<!-- End Google Tag Manager -->
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Log In &lsaquo; Cuci Walet &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
